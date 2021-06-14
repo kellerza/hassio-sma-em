3ce06bc2d4d4b1883383ae7841210b39d752fb0e
Johann Kellerman <kellerza@gmail.com>
2021-06-14 23:51:56 +0200

    async read

--- sma-em-dev/run.py
+++ sma-em-dev/run.py
@@ -1,185 +1,55 @@
-#!/usr/bin/env python3
-"""Run the energy meter."""
+import asyncio
+import logging
 import socket
 import struct
-import time
-import traceback
-from contextlib import contextmanager
-from json import dumps, loads
-from pathlib import Path
 
-import attr
-import paho.mqtt.client as mqtt
-from icecream import ic
+import sensors
 from speedwiredecoder import decode_speedwire
 
-MCAST_GROUP = "239.12.255.254"
-MCAST_PORT = 9522
-BIND_IP = "0.0.0.0"
-
-MQTT_HOST = "MQTT_HOST"
-MQTT_PORT = "MQTT_PORT"
-MQTT_USERNAME = "MQTT_USERNAME"
-MQTT_PASSWORD = "MQTT_PASSWORD"
-SERIALS = "SMA_SERIALS"
-FIELDS = "FIELDS"
-
-SMA_EM_TOPIC = "SMA-EM/status"
+BROADCAST_ADDR = "239.12.255.254"
+BROADCAST_PORT = 9522
 
 
-@contextmanager
-def mqtt_client(opt):
-    """Connect to the MQTT broker provide the client."""
-    client = mqtt.Client(client_id="sma-em-hassos")
-    client.username_pw_set(username=opt[MQTT_USERNAME], password=opt[MQTT_PASSWORD])
-    sleep = 5
-    while True:
+class MulticastServerProtocol:
+    def connection_made(self, _transport):
+        pass
+
+    def datagram_received(self, data, _addr):
         try:
-            client.connect(opt[MQTT_HOST], opt[MQTT_PORT])
-            client.loop_start()  # threaded
-            break
-        except Exception as e:
-            print("Could not connect to the MQTT client:", str(e))
-            time.sleep(sleep)
-            if sleep < 60:
-                sleep += 10
-
-    try:
-        yield client
-    finally:
-        client.loop_stop()
-        client.disconnect()
-
-
-def yield_sma_emparts():
-    """Receive multicast Speedwire datagrams."""
-    while True:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("", MCAST_PORT))
-        try:
-            mreq = struct.pack(
-                "4s4s", socket.inet_aton(MCAST_GROUP), socket.inet_aton(BIND_IP)
-            )
-            sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-            print("Multicast group connected")
-        except BaseException:
-            print("Could not connect to the mulicast group... will retry in 5s")
-            time.sleep(5)
+            emparts = decode_speedwire(data)
+        except Exception as err:
+            logging.getLogger(__name__).warning("Could not decode Speedwire %s", err)
         else:
-            try:
-                while True:
-                    yield decode_speedwire(sock.recv(608))
-            finally:
-                print("closing socket")
-                sock.close()
-
-
-# device_calss:current/energy/power
-# unit_of_measurement
-def hass_device_class(name):
-    if name.endswith("counter"):
-        return "energy"
-    if name.startswith("u"):
-        return "voltage"
-    if name.endswith("frequency"):
-        return None
-    return "power"
-
-
-def hass_discover_sensor(*, client, sma_id, field):
-    smap = ("", "", "")  # SENSOR_MAP.get(field, ("energy", ""))
-    topic = f"homeassistant/sensor/{sma_id}/{field}/config"
-    payload = dumps(
-        {
-            "name": field,
-            "dev_cla": smap[0],
-            "stat_t": f"{SMA_EM_TOPIC}/{sma_id}/{field}",
-            "unit_of_meas": smap[1],
-            "uniq_id": f"{sma_id}_{field}",
-            "dev": {
-                "ids": [f"sma_em_{sma_id}"],
-                "name": "SMA Energy Meter",
-                "mdl": "Energy Meter",
-                "mf": "SMA",
-            },
-        }
-    )
-
-    ic(topic, payload)
-    print("")
-    client.publish(topic, payload, retain=True)
-
-
-@attr.define
-class SWSensor:
-    """A speedwire sensor."""
-
-    name = attr.field()
-    mod = attr.field()  # 0 = mean, 1 = max, -1 = min
-    last = attr.field(default=0)
-    interval = attr.field(default=5)
-    values = attr.field(factory=list)
-    unit = attr.field(default=None)
-    device_class = attr.field(default=None)
-
-
-THRESHOLD_ABS = 70  # W
-THRESHOLD_REL = 100  # %
-
-
-def get_sensors(definition, emparts):
-    res = []
-    for sensor_def in definition:
-        name, _, mod = sensor_def.partition(":")
-
-        if name not in emparts:
-            print("Unknown sensor", name)
-            continue
-
-        sen = SWSensor(name=name, mod=mod, unit=emparts.get(f"{name}unit"))
-        if sen.mod in ["min", "max"]:
-            sen.interval = 60
-        else:
-            try:
-                sen.interval = int(mod)
-                sen.mod = "avg"
-            except ValueError:
-                sen.mod = "smart"
-                sen.interval = 50
-        sen.device_class = hass_device_class(name)
-        res.append(sen)
-
-    ic(res)
-    return res
-
-
-def process_sensors(sensors, emparts):
-    pass
+            asyncio.get_event_loop().create_task(sensors.process_emparts(emparts))
 
 
 def main():
-    # Read the hassos configuration
-    options = loads(Path("/data/options.json").read_text())
+    LOOP = asyncio.get_event_loop()
+    LOOP.set_debug(True)
+    logging.basicConfig(level=logging.DEBUG)
 
-    sensors = {}
-    while True:
-        with mqtt_client(options) as client:
-            try:
-                for emparts in yield_sma_emparts():
-                    serial = emparts["serial"]
-                    if not sensors.get(serial):
-                        sensors[serial] = get_sensors(options[FIELDS], emparts)
-                        for sen in sensors[serial]:
-                            hass_discover_sensor(
-                                client=client, sma_id=serial, field=sen.name
-                            )
-                    process_sensors(client, emparts, sensors[serial])
-            except Exception as err:
-                print("Daemon: Exception occurred", err)
-                print(traceback.format_exc())
-                return  # will attempt to rebind the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", BROADCAST_PORT))
+    group = socket.inet_aton(BROADCAST_ADDR)
+    mreq = struct.pack("4sL", group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    sensors.startup()
+
+    listen = LOOP.create_datagram_endpoint(
+        MulticastServerProtocol,
+        sock=sock,
+    )
+    print("done")
+
+    _transport, _protocol = LOOP.run_until_complete(listen)
+
+    LOOP.run_forever()
+    LOOP.close()
 
 
 if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     main()
---
+++ sma-em-dev/sensors.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+"""Run the energy meter."""
+import asyncio
+import statistics
+import sys
+import time
+from json import dumps, loads
+from pathlib import Path
+from typing import Any, Dict, Sequence
+
+import attr
+from asyncio_mqtt import Client
+from icecream import ic
+
+
+@attr.define
+class SWSensor:
+    """A speedwire sensor."""
+
+    name = attr.field()
+    mod = attr.field()  # 0 = mean, 1 = max, -1 = min
+    last_update: int = attr.field(default=0)
+    interval = attr.field(default=5)
+    value = attr.field(default=0)
+    values = attr.field(factory=list)
+    unit = attr.field(default=None)
+    device_class = attr.field(default=None)
+
+
+SENSORS: Dict[str, Sequence[SWSensor]] = {}
+OPTIONS: Dict[str, Any] = {}
+MQTT: Client = None
+
+
+MQTT_HOST = "MQTT_HOST"
+MQTT_PORT = "MQTT_PORT"
+MQTT_USERNAME = "MQTT_USERNAME"
+MQTT_PASSWORD = "MQTT_PASSWORD"
+SERIALS = "SMA_SERIALS"
+FIELDS = "FIELDS"
+THRESHOLD = "THRESHOLD"
+
+SMA_EM_TOPIC = "SMA-EM/status"
+
+
+async def process_emparts(emparts: dict):
+    serial = emparts["serial"]
+    if not SENSORS.get(serial):
+        SENSORS[serial] = get_sensors(definition=OPTIONS[FIELDS], emparts=emparts)
+        for sen in SENSORS[serial]:
+            await hass_discover_sensor(sma_id=serial, field=sen.name)
+
+    push_later = []
+    now = int(time.time())
+    for sen in SENSORS[serial]:
+        val = emparts.get(sen.name)
+
+        publish = now >= sen.last_update + sen.interval
+
+        # check threshold crossing
+        smart_s = sen.mod == "" and sen.values
+        if smart_s and val > sen.value + OPTIONS[THRESHOLD]:
+            publish = True
+            push_later.append((sen, val, OPTIONS[THRESHOLD]))
+        elif smart_s and val < sen.value - 2 * OPTIONS[THRESHOLD]:
+            publish = True
+            push_later.append((sen, val, -2 * OPTIONS[THRESHOLD]))
+        else:
+            sen.values.append(val)
+
+        if not publish:
+            continue
+        ic(now, val, sen.value, len(sen.values))
+
+        sen.last_update = now
+        if sen.mod == "min":
+            sen.value = round(min(sen.values), 1)
+        if sen.mod == "max":
+            sen.value = round(max(sen.values), 1)
+        else:
+            sen.value = round(statistics.mean(sen.values), 1)
+        sen.values = []
+
+        ic(sen.name, sen.value)
+        await MQTT.publish(
+            f"{SMA_EM_TOPIC}/{emparts['serial']}/{sen.name}{sen.mod}", sen.value
+        )
+
+    if not push_later:
+        return
+
+    await asyncio.sleep(0.005)
+    for sen, val, delta in push_later:
+        sen.value = round(val, 1)
+        ic(sen.name, sen.value, delta)
+        await MQTT.publish(
+            f"{SMA_EM_TOPIC}/{emparts['serial']}/{sen.name}{sen.mod}", sen.value
+        )
+
+
+# device_calss:current/energy/power
+# unit_of_measurement
+def hass_device_class(name: str):
+    if name.endswith("counter"):
+        return "energy"
+    if name.startswith("u"):
+        return "voltage"
+    if name.endswith("frequency"):
+        return None
+    return "power"
+
+
+async def hass_discover_sensor(*, sma_id: str, field: str):
+    smap = ("", "", "")  # SENSOR_MAP.get(field, ("energy", ""))
+    topic = f"homeassistant/sensor/{sma_id}/{field}/config"
+    payload = dumps(
+        {
+            "name": field,
+            "dev_cla": smap[0],
+            "stat_t": f"{SMA_EM_TOPIC}/{sma_id}/{field}",
+            "unit_of_meas": smap[1],
+            "uniq_id": f"{sma_id}_{field}",
+            "dev": {
+                "ids": [f"sma_em_{sma_id}"],
+                "name": "SMA Energy Meter",
+                "mdl": "Energy Meter",
+                "mf": "SMA",
+            },
+        }
+    )
+
+    ic(topic, payload)
+    print("")
+    await MQTT.connect()
+    await MQTT.publish(topic, payload, retain=True)
+
+
+def get_sensors(*, definition: str, emparts: dict):
+    res = []
+    for sensor_def in definition:
+        name, _, mod = sensor_def.partition(":")
+
+        if name not in emparts:
+            print("Unknown sensor", name)
+            continue
+
+        sen = SWSensor(name=name, mod=mod, unit=emparts.get(f"{name}unit"))
+        if sen.mod in ["min", "max"]:
+            sen.interval = 60
+        else:
+            try:
+                sen.interval = int(mod)
+                sen.mod = "avg"
+            except ValueError:
+                sen.mod = ""
+                sen.interval = 60
+        sen.device_class = hass_device_class(name)
+        res.append(sen)
+
+    ic(res)
+    return res
+
+
+def startup():
+    """Read the hassos configuration."""
+    try:
+        options = loads(Path("/data/options.json").read_text())
+    except FileNotFoundError:
+        options = {
+            MQTT_HOST: "192.168.88.128",
+            MQTT_PASSWORD: sys.argv[1],
+            MQTT_PORT: 1883,
+            MQTT_USERNAME: "hass",
+            SERIALS: [],
+            FIELDS: ["pconsume"],
+            THRESHOLD: 80,
+            "DEBUG": 0,
+        }
+    for key, val in options.items():
+        OPTIONS[key] = val
+
+    global MQTT
+    MQTT = Client(
+        hostname=OPTIONS[MQTT_HOST],
+        port=OPTIONS[MQTT_PORT],
+        username=OPTIONS[MQTT_USERNAME],
+        password=OPTIONS[MQTT_PASSWORD],
+    )
