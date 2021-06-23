7bbf5ee01816e77f7690461133fb2b3dcb724bb1
Patrick Beer <patrick@pabera.com>
2021-06-23 09:26:13 +0200

    Introduce socket reconnection interval

--- sma-em/run.py
+++ sma-em/run.py
@@ -10,6 +10,9 @@ from speedwiredecoder import decode_speedwire
 
 BROADCAST_ADDR = "239.12.255.254"
 BROADCAST_PORT = 9522
+RECONNECT_INTERVAL = 180
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class MulticastServerProtocol(asyncio.DatagramProtocol):
@@ -20,12 +23,21 @@ class MulticastServerProtocol(asyncio.DatagramProtocol):
         try:
             emparts = decode_speedwire(data)
         except Exception as err:
-            logging.getLogger(__name__).warning("Could not decode Speedwire %s", err)
+            _LOGGER.warning("Could not decode Speedwire %s", err)
         else:
             asyncio.get_running_loop().create_task(sensors.process_emparts(emparts))
 
+def connect_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", BROADCAST_PORT))
+    group = socket.inet_aton(BROADCAST_ADDR)
+    mreq = struct.pack("4sL", group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT,)
+    return sock
 
-def main():
+async def main():
     sensors.startup()
     logging.basicConfig(
         level=logging.DEBUG if sensors.OPTIONS["DEBUG"] != 0 else logging.INFO
@@ -34,24 +46,20 @@ def main():
     LOOP = asyncio.get_event_loop()
     LOOP.set_debug(True)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("", BROADCAST_PORT))
-    group = socket.inet_aton(BROADCAST_ADDR)
-    mreq = struct.pack("4sL", group, socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    while True:
+        _sock = connect_socket()
+        _transport, _protocol = await LOOP.create_datagram_endpoint(
+            MulticastServerProtocol,
+            sock=_sock,
+        )
 
-    listen = LOOP.create_datagram_endpoint(
-        MulticastServerProtocol,
-        sock=sock,
-    )
-    _transport, _protocol = LOOP.run_until_complete(listen)
-    LOOP.run_forever()
-    LOOP.close()
-
+        try:
+            await asyncio.sleep(RECONNECT_INTERVAL)
+        finally:
+            _transport.close()
+            _sock.close()
 
 if __name__ == "__main__":
     # if os.name == "nt":
     #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    main()
+    asyncio.run(main())
