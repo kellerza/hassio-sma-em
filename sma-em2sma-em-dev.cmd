2723e1ec1f31434c8367e37e18c637a58152d226
Johann Kellerman <kellerza@gmail.com>
2021-09-21 22:10:57 +0200

    dev version

--- sma-em2sma-em-dev.cmd
+++ sma-em2sma-em-dev.cmd
@@ -1,3 +1,5 @@
 xcopy /y /s sma-em sma-em-dev
 sed -i 's/sma-em"/sma-em-dev"/g' sma-em-dev/config.json
 sed -i 's/"SMA Energy Meter"/"SMA Energy Meter *developer version*"/' sma-em-dev/config.json
+sed -i 's/# SMA Energy Meter Add-On/# SMA Energy Meter Add-On *developer version*/' sma-em-dev/README.md
+sed -i 's/# Configuration/# Configuration *developer version*/' sma-em-dev/DOCS.md
